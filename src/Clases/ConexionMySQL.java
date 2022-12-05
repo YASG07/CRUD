a86b3255b9a21dd6f3cc7ba64d89f2f9260c7abd
Yael SG <yaeldh8@gmail.com>
2022-12-04 19:11:47 -0600

    first version

---
+++ src/Clases/ConexionMySQL.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import javax.swing.JFrame;
+import static javax.swing.JOptionPane.showMessageDialog;
+//Importaciones para conectar con mysql
+/**
+ *
+ * @author Lenovo
+ */
+public class ConexionMySQL {
+    
+    public static Connection con;
+    public static Statement st;
+    
+    public static void CargarBD(){
+        
+    }//metodo conectar a base de datos
+    
+}
